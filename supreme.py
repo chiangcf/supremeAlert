ac6587a65213308e33d278c6d86275419e9e7540
Christian <christiandcf@gmail.com>
2017-06-08 00:07:01 -0500

    Added Names

--- supreme.py
+++ supreme.py
@@ -16,11 +16,13 @@ uClient.close()
 soup = BeautifulSoup(html, "html.parser")
 # print(page_soup.body.span)
 
-# finding the graphics cards, item-container is the name of the class
+# finding the clothing piece, item-container is the name of the class
 containers = soup.findAll("div", {"class":"inner-article"})
 
 # print(containers[0])
-available_list = []
+# available_list = []
+
+
 
 # Item object
 class Item:
@@ -29,26 +31,43 @@ class Item:
       self.link = link
 
     def displayItem(self):
-      print ("Name : " + self.name + " Link: " + self.link)
+      print (self.name + ":  " + self.link)
 
-item1 = Item("Fuck", "google.com")
-item1.displayItem()
+# Stores all the items
+all_items = [];
 
 # it just go through the html tree
-# for container in containers:
-#     item = container.a
-#
-#     # item_url = item["href"]
-#
-#     temp = item.div #this will detect the sold out
-#
-#     # if the division is not present then it will add it to the availble item list
-#     if temp == None:
-#         if item["href"] not in available_list:
-#             available_list.append(item["href"])
-#             available_url = item["href"] #variables of only available
-#             available_item = "http://supremenewyork.com" + available_url
-#             # print(available_item)
+for container in containers:
+    item = container.a
+
+
+    temp = item.div #this will detect the sold out
+
+    # if the division is not present then it will add it to the availble item list
+    if temp == None:
+        available_url = item["href"] #variables of only available
+        available_item_url = "http://supremenewyork.com" + available_url
+
+        # opens the connection
+        uClient2 = uReq(available_item_url)
+        html2 = uClient2.read()
+        uClient2.close()
+
+        #html parsing
+        soup2= BeautifulSoup(html2, "html.parser")
+
+        # find the div with name
+        containers2 = soup2.find("div", {"id":"details"})
+        available_item_name = containers2.h1.get_text()
+        available_item_color = containers2.p.get_text();
+
+        if(available_item_color != None):
+            available_item_name = containers2.h1.get_text() + "(" + available_item_color + ")"
+
+        all_items.append(Item(available_item_name, available_item_url))
+
+for i in all_items:
+    i.displayItem()
 
 
 print("done")
