8999f0995c7d03c6b9b12956f8ccdb5c87d48094
Christian <christiandcf@gmail.com>
2017-06-08 22:34:41 -0500

    Added Twitter

--- supreme.py
+++ supreme.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen as uReq
+import tweepy
+
+
+# Getting Info.
 
 my_url = "http://www.supremenewyork.com/shop/all"
 
@@ -14,13 +18,11 @@ uClient.close()
 
 #html parsing
 soup = BeautifulSoup(html, "html.parser")
-# print(page_soup.body.span)
 
 # finding the clothing piece, item-container is the name of the class
 containers = soup.findAll("div", {"class":"inner-article"})
 
-# print(containers[0])
-# available_list = []
+
 
 
 
@@ -33,6 +35,8 @@ class Item:
     def displayItem(self):
       print (self.name + ":  " + self.link)
 
+    def insideItem(self):
+        return "Name: " + self.name
 # Stores all the items
 all_items = [];
 
@@ -61,13 +65,48 @@ for container in containers:
         available_item_name = containers2.h1.get_text()
         available_item_color = containers2.p.get_text();
 
-        if(available_item_color != None):
-            available_item_name = containers2.h1.get_text() + "(" + available_item_color + ")"
+        if(available_item_color != ' '):
+            available_item_name = containers2.h1.get_text() + " (" + available_item_color + ")"
 
         all_items.append(Item(available_item_name, available_item_url))
 
-for i in all_items:
-    i.displayItem()
+# Displays all the items
+# for i in all_items:
+#     i.displayItem()
+
+
+# rip = all_items[0].name + " " + all_items[0].link
+# print(rip)
+
+
+# Twitter stuff
+
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+def tweeting():
+  # Fill in the values noted in previous step here
+  cfg = {
+    "consumer_key"        : "UEzbGhVjbtXwtnWHPKhRz00j7",
+    "consumer_secret"     : "LVVqCduMaJJX3QbtEJmR89SHbnxTq0ysKip7oGDtAuFXnmMF6W",
+    "access_token"        : "873013019579756544-D0FeNqnV7M9WvegayfqqG48bpDtu9kk",
+    "access_token_secret" : "XIQGHw8CnSgTRZpKrZd7Whs5XvVYH5IMPAM049pyCaYgK"
+    }
+
+  api = get_api(cfg)
+
+  for i in all_items:
+
+      tweet = i.name + " " + i.link
+      status = api.update_status(status=tweet)
+  # Yes, tweet is called 'status' rather confusing
+
+# if __name__ == "__main__":
+#   main()
+
+tweeting()
 
 
 print("done")
